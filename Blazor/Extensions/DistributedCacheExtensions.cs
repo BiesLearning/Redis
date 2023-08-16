d2ab0031bcde3be9ff2e611bf77cbe1d3057b7bf
bies <bies@DESKTOP-VKKSGUG>
2023-08-16 11:29:59 +0300

    Add project files.

---
+++ Blazor/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Blazor.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task SetAsync<T>(this IDistributedCache cache, string key, T value, TimeSpan? absoluteExpireTime = null, TimeSpan? unusedExpireTime = null)
+        {
+            DistributedCacheEntryOptions options = new()
+            {
+                AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60),
+                SlidingExpiration = unusedExpireTime
+            };
+            var json = JsonSerializer.Serialize(value);
+            await cache.SetStringAsync(key, json, options);
+        }
+        public static async Task<T?> GetAsync<T>(this IDistributedCache cache, string key)
+        {
+            var json = await cache.GetStringAsync(key);
+            return json is null ? default : JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
